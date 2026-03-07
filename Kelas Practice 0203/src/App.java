0ea5afd971d6d5826ad40f7c03865ecd46fc93dc
keisyaahm <keisyaahm@users.noreply.github.com>
2026-03-07 22:42:37 +0700

    Update isi folder

--- Kelas Practice 0203/src/App.java
+++ Kelas Practice 0203/src/App.java
@@ -1,14 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        //inisiasi
+        //inisiasi object
         Car myCar = new Car();
-
         Car myHonda = new Car("Honda", "Brio", "Kuning", 1500);
-
     
         System.out.println(myCar.getBrand()); //ini buat manggil default
-        System.out.println(myHonda.getBrand());
+        System.out.println(myHonda.getBrand()); //memanggil nilai dr constructor
 
+        //setter untuk mengubah nilai atribut, getter untuk mengambil nilai atribut
         myCar.setBrand("Toyota");
         System.out.println(myCar.getBrand());
 
@@ -17,33 +16,39 @@ public class App {
 }
 
 class Car {
-    //atribut
-    //pivate tdk bisa di akses di luar kelass, kalo bisa pakai setter getter. tiap atribut bikin set dan get
+    //ENCAPSULATION
+    //atribut private
+    //pivate tdk bisa di akses di luar kelass, kalo bisa pakai setter getter. tiap atribut bikin set dan get biar ga diubah sembarangan
     private String brand; //string itu sebenarnya kelas, kalo kecil nanti error
     private String type;
     private String color;
     private int speed;
 
-    // constructor gak perlu return type,  gak bisa ditumpuk/diwariskan, ada 3 jenis
+    //POLIMORPHISM
+    //constructor gak perlu return type,  gak bisa ditumpuk/diwariskan, ada 3 jenis
     public Car(){ //tipe 1 no argumen
         this.brand = "Default";
     }
 
+        //constructor dengan parameter
         public Car(String brand, String type, String color, int speed){
             this.brand = brand;
             this.type = type;
             this.color = color;
             this.speed = speed;
 
-        } //type 2 ada paratemeternya
-        //type 3 itu impilisit 
+        }
+        //type 3 itu impilisit  
 
-    // kalo method hrs ada return type
-    // method biasa
+    //kalo method hrs ada return type
+    //method biasa
+    //ini gunanya untuk mengambil nilai atribut brand, karena brand itu private jadi harus pake getter
     public String getBrand(){ //public supaya bisa digunakan diluar kelas
         return brand;
     }
 
+
+    //setter untuk mengubah nilai atribut brand, karena brand itu private jadi harus pake setter
     public void setBrand(String  brandNew){
         this.brand = "Xenia";
     }
--- Kelas Practice 0203/src/App2.java
+++ Kelas Practice 0203/src/App2.java
@@ -1,20 +1,25 @@
 public class App2 {
     public static void main(String[] args) throws Exception {
-        
+        //inisiasi object
         ElectricCar myCar = new ElectricCar("Tesla", "Brio", "Merah", 0);
 
         System.out.println(myCar.getSpeed());
+
+        
         myCar.startEngine();
 }
 }
 
+
+//ABSTRACT 
+//ini gak bs di instansiasi bs diturunin ke kls lain
 abstract class Car{
     protected String brand;
     protected String type;
     protected String color;
-    private int speed;
+    private int speed; //encapsulation, biar tdk bs diakses dr luar, kalo mau akses pake getter setter
 
-    // constructor
+    //constructor super class
     public Car (String brand, String type, String color, int speed){
             this.brand = brand;
             this.type = type;
@@ -22,29 +27,33 @@ abstract class Car{
             this.speed = speed;
     }
 
-    // method abstrak gak perlu kurawal
+    //method abstrak gak perlu kurawal
     abstract void startEngine();
 
-    // encapsulation, melindungi stribut agar tdk bs diakses dr luar
-    //setter getter
+    //encapsulation, melindungi stribut agar tdk bs diakses dr luar
+    //getter
     public int getSpeed(){ //public supaya bisa digunakan diluar kelas
         return speed;
     }
 
-    public void setSpeed(String speedNew){
-        this.brand = speedNew;
+    //setter
+    public void setSpeed(int speedNew){
+        this.speed = speedNew;
     }
 
 }
 //protected bs diakses kls turunan, pvt tdk
 
 
+//INHERITANCE/waris
 class ElectricCar extends Car{ //ini itu buat mewarisi, kalo extends itu buat mewarisi, kalo implements itu buat interface
-    // constructur
+    //constructor sub class
     public ElectricCar (String brand, String type, String color, int speed){
         super (brand, type, color, speed);
     }
 
+
+    //POLIMORPHISM
     @Override //mbl listrik disel kan beda implementasi, maka ada abstrak method kan mobil pasti bisal jalan
     //melakukan hal yg sama dg cara beda
     void startEngine(){
